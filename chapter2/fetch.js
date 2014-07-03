1824f1de8f86af76d88b9e9834accddc629e0b97
Felicia Larion <felicialarion@reea.net>
2014-07-03 17:10:23 +0300

    http as client

---
+++ chapter2/fetch.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var urlOpts = {host: 'www.nodejs.org', path: '/', port: '80'};
+http.get(urlOpts, function(res){
+	res.on('data', function(chunk){
+		console.log(chunk.toString());
+	});
+})
